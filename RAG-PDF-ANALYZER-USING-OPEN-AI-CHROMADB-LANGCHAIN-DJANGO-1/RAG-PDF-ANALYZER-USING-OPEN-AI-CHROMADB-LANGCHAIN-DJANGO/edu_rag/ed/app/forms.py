d45b48e403a80b41301d7bd6500875ec176777a6
VarunKumar-M <varunkumarm2494@gmail.com>
2024-10-26 11:06:03 +0530

    Delete RAG-PDF-ANALYZER-USING-OPEN-AI-CHROMADB-LANGCHAIN-DJANGO-1 directory

--- RAG-PDF-ANALYZER-USING-OPEN-AI-CHROMADB-LANGCHAIN-DJANGO-1/RAG-PDF-ANALYZER-USING-OPEN-AI-CHROMADB-LANGCHAIN-DJANGO/edu_rag/ed/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import PDFDocument
-
-class PDFUploadForm(forms.ModelForm):
-    class Meta:
-        model = PDFDocument
-        fields = ['title', 'pdf_file']
